57009face0cdc5f16928f5383b08afcc18a4e547
decent-port-4955 <emil@georgetownoliveoil.com>
2019-09-20 17:48:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ function prependKitten(name){
   return [name,...kittens]
 }
 
-function removeLastKitten(name){
-  return kitten.slice(0,kittens.length-1)
+function removeLastKitten(){
+    return kittens.slice(0,kittens.length-1)
   
 }
 
