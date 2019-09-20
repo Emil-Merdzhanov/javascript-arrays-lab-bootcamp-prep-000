9f51e91265e03c7dfcdc657a181f4e5046156edc
decent-port-4955 <emil@georgetownoliveoil.com>
2019-09-20 17:35:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ function destructivelyAppendKitten(name){
   return kittens.push(name) 
 }
 
-function destructivelyPrependKitten(name){
-  return kittens.unshift(name)
-}
-
 function destructivelyRemoveLastKitten(name){
   return kittens.pop(name)
 }
