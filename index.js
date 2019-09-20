b33961e3774bcd665f96fdede86a91c9bdffad15
decent-port-4955 <emil@georgetownoliveoil.com>
2019-09-20 17:28:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
-function destructivelyAppendKitten(name)
+function destructivelyAppendKitten(name){
+  array.destructivelyAppendKitten
+}
 // Add your functions and code here
